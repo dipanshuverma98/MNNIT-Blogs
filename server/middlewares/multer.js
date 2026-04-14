cff52fbe70474f7017349b54ee2b87bb3c6aa392
dipanshuverma98 <dipanshu9879@gmail.com>
2026-04-14 14:48:34 +0530

    addblog

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,5 +1,7 @@
 import multer from "multer";
 
-const upload = multer({storage: multer.diskStorage({})});
+const storage = multer.memoryStorage();
+
+const upload = multer({ storage: storage });
 
 export default upload;
